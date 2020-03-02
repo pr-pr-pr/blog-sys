3cb5fbb4d4961f1bafd77c9a945f26ea19436774
hhp1614 <hhp1614@gmail.com>
2020-03-02 15:08:13 +0800

    update(server): 博客系统前台展示（服务端）基本完成

--- server/apps/server/src/main.ts
+++ server/apps/server/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
-  console.log(3000)
+
+  const options = new DocumentBuilder()
+    .setTitle('博客系统 - 客户端 API')
+    .setDescription('供客户端界面调用的服务端API')
+    .setVersion('1.0')
+    .build()
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('api-docs', app, document)
+
+  const PORT = process.env.SERVER_PORT || 5001
+  await app.listen(PORT)
+  console.log(`客户端 API 文档地址：http://localhost:${PORT}/api-docs`)
 }
 bootstrap()
