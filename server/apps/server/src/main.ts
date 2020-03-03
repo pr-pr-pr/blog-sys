1504e1a77daef36c755cdbde8ae16421538d4730
hhp1614 <hhp1614@gmail.com>
2020-03-04 01:34:52 +0800

    feat: 增加文件上传接口，配置静态文件托管

--- server/apps/server/src/main.ts
+++ server/apps/server/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors();
+  app.useStaticAssets('uploads', { prefix: '/uploads' });
 
   const options = new DocumentBuilder()
     .setTitle('博客系统 - 客户端 API')
