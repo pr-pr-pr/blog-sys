8f705d1f95c64b1494bb5332789c64e6f9a1ce6c
hhp1614 <hhp1614@gmail.com>
2020-03-12 19:03:41 +0800

    style(admin-react): 全局声明文件添加注释

--- admin-react/src/global.d.ts
+++ admin-react/src/global.d.ts
@@ -1,19 +1,31 @@
-declare interface CommonQueryParams {
+/**
+ * 列表接口公共参数类型（分页和排序）
+ */
+declare interface CommonQueryParams extends SortParams {
   page?: number;
   limit?: number;
-  sort?: number;
+  sort?: -1 | 1;
   sortKey?: string;
 }
 
+/**
+ * 列表接口排序参数类型
+ */
 declare interface SortParams {
   sort?: -1 | 1;
   sortKey?: string;
 }
 
+/**
+ * Antd 表格 onChange 事件方法类型
+ */
 declare interface AntdTableChange {
   (...args: any): void;
 }
 
+/**
+ * 表格数据类型
+ */
 declare interface TableData<T = any> {
   list: T[];
   page: number;
